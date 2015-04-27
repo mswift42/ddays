cae63277783a13e14ea0f7ff12c88698c2dfe7e5
martin.haesler <martin.haesler@gmail.com>
2015-04-27 17:33:12 +0100

    Add tests for class initialisation.

--- test/testsingletask.dart
+++ test/testsingletask.dart
@@ -7,6 +7,10 @@ void main() {
   test('task gets initialised correctly', () {
     var t1 = new Task('task1',null, '');
     expect(t1.summary, 'task1');
+    var t2 = new Task('task2', new DateTime(2015,1,1), 'work');
+    expect(t2.scheduled.month, 1);
+    expect(t2.status, 'work');
   });
+ 
 }
     
