799e9ad25419142340cecbd93e9ae02ceaf2b17f
martin.haesler <martin.haesler@gmail.com>
2015-04-27 18:25:12 +0100

    Add test for addnotes

--- test/testsingletask.dart
+++ test/testsingletask.dart
@@ -5,11 +5,15 @@ import "package:test/test.dart";
 
 void main() {
   test('task gets initialised correctly', () {
-    var t1 = new Task('task1',null, '');
+    var t1 = new Task('task1',null);
     expect(t1.summary, 'task1');
-    var t2 = new Task('task2', new DateTime(2015,1,1), 'work');
+    var t2 = new Task('task2', new DateTime(2015,1,1));
     expect(t2.scheduled.month, 1);
-    expect(t2.status, 'work');
+  });
+  test('addNote should add notes to tasknotes list.', () {
+    var t1 = new Task('task1', null);
+    t1.addNote('first note');
+    expect(t1.tasknotes[0], 'first note');
   });
  
 }
