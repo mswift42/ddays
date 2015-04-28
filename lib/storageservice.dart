2e3e37516d5c5fa0065fdd09c8ca748b3d6aaeae
martin.haesler <martin.haesler@gmail.com>
2015-04-28 19:19:53 +0100

    Add saveToStorage function.

--- lib/storageservice.dart
+++ lib/storageservice.dart
@@ -1,1 +1,11 @@
 library storageservice;
+
+import 'dart:html';
+import 'dart:convert';
+
+
+void saveToStorage(storedlist, item, name) {
+  storedlist.add(item);
+  window.localStorage[name] = JSON.encode(item);
+}
+
