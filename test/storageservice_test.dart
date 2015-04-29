5e620babaa9af1470e872ae241ada648756563da
martin.haesler <martin.haesler@gmail.com>
2015-04-29 20:30:05 +0100

    Add initstorage function for tests

--- test/storageservice_test.dart
+++ test/storageservice_test.dart
@@ -1,3 +1,13 @@
 
 import 'package:ddays/storageservice.dart';
 import 'package:test/test.dart';
+import 'dart:html';
+import 'dart:convert';
+
+void initStorage() {
+  window.localStorage[STORAGE_KEY] = JSON.encode([]);
+}
+  
+
+void main() {
+  test('
