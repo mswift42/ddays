21a99460e112ea6bea1274296edc4fbb40a58163
martin.haesler <martin.haesler@gmail.com>
2015-04-29 14:22:50 +0100

    Factor out name dependency.

--- lib/storageservice.dart
+++ lib/storageservice.dart
@@ -3,13 +3,22 @@ library storageservice;
 import 'dart:html';
 import 'dart:convert';
 
+final String STORAGE_KEY = 'ddays';
 
-void saveToStorage(storedlist, item, name) {
+// add an item to a given List, then save
+// this list in localStorage with the key 'STORAGE_KEY'.
+void saveToStorage(storedlist, item) {
   storedlist.add(item);
-  window.localStorage[name] = JSON.encode(item);
+  window.localStorage[STORAGE_KEY] = JSON.encode(item);
 }
 
-List loadFromStorage(name) {
-  return JSON.decode(window.localStorage[name]);
+// restore a to localstorage, under key 'STORAGE_KEY',
+// saved List.
+List loadFromStorage() {
+  return JSON.decode(window.localStorage[STORAGE_KEY]);
+}
+
+// delete a local Storage object with key 'STORAGE_KEY'.
+void deleteItemInStorage(storedlist, item) {
 }
 
