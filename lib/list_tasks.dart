44ea64a66d7cbc86bf0fa23b7e0f068cda466f8a
martin.haesler <martin.haesler@gmail.com>
2015-05-02 10:13:36 +0100

    Add listtasks function.

--- lib/list_tasks.dart
+++ lib/list_tasks.dart
@@ -1,2 +1,15 @@
 library list_tasks;
 
+import 'dart:html';
+
+LIElement singletask(String tasksummary) {
+  LIElement li = new LIElement();
+  ParagraphElement p = new ParagraphElement();
+  p
+   ..text = tasksummary
+    ..classes.add('summarytext');
+  li
+    ..classes.add('singletask')
+    ..append(p);
+  return li;
+}
