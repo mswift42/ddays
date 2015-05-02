1b909bb0f198ddae8b764acc47972e452663834c
martin.haesler <martin.haesler@gmail.com>
2015-05-02 15:40:59 +0100

    Refresh task list view when new task is submitted

--- lib/new_task.dart
+++ lib/new_task.dart
@@ -3,11 +3,17 @@ library new_task;
 import 'dart:html';
 import 'package:ddays/task.dart' show Task;
 import 'package:ddays/storageservice.dart' show saveToStorage, STORAGE_KEY;
+import 'package:ddays/list_tasks.dart' show displayTasks;
+
 
 void saveTask(Event e) {
   e.preventDefault();
   InputElement summary = querySelector('[name="tsummary"]');
   Task task = new Task(summary.value.trim());
+  saveToStorage({"summary": task.summary,
+    "scheduled" : task.scheduled, "timestamp" : task.timestamp});
+  summary.value = '';
+  displayTasks();
   
   
 }
