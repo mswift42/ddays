7914843a45304531594cbe52ab9e56d0c6a7f7e3
martin.haesler <martin.haesler@gmail.com>
2015-05-03 09:37:10 +0100

    Refactor saveTask into saveTaskHandler.

--- lib/new_task.dart
+++ lib/new_task.dart
@@ -2,12 +2,13 @@ library new_task;
 
 import 'dart:html';
 import 'package:ddays/task.dart' show Task;
-import 'package:ddays/storageservice.dart' show saveToStorage, STORAGE_KEY;
+import 'package:ddays/storageservice.dart' show saveToStorage, tasks;
 import 'package:ddays/list_tasks.dart' show displayTasks;
 
 
-void saveTask(Event e) {
+void saveTaskHandler(Event e) {
   e.preventDefault();
+  print(e);
   InputElement summary = querySelector('[name="tsummary"]');
   Task task = new Task(summary.value.trim());
   saveToStorage({"summary": task.summary,
