ee682ddb3d64069b9309b3a0fb2de036eb312786
martin.haesler <martin.haesler@gmail.com>
2015-05-03 12:03:36 +0100

    Insert newly created task

--- lib/new_task.dart
+++ lib/new_task.dart
@@ -2,21 +2,20 @@ library new_task;
 
 import 'dart:html';
 import 'package:ddays/task.dart' show Task;
-import 'package:ddays/storageservice.dart' show saveToStorage, tasks;
-import 'package:ddays/list_tasks.dart' show displayTasks;
+import 'package:ddays/storageservice.dart' show saveToStorage, tasklist;
+import 'package:ddays/list_tasks.dart' show insertTask;
 
 
 void saveTaskHandler(Event e) {
   e.preventDefault();
   InputElement summary = querySelector('[name="tsummary"]');
-  print(tasks);
   if (summary.value.trim() != '') {
   Task task = new Task(summary.value.trim());
   saveToStorage({"summary": task.summary,
     "scheduled" : task.scheduled, "timestamp" : task.timestamp});
+  summary.value = '';
+  insertTask(task.summary);
   } 
   summary.value = '';
-  displayTasks();
-  
   
 }
