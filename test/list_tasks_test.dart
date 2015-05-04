156d1ef4469aabc75b3d80c61525cdd4acd9528e
martin.haesler <martin.haesler@gmail.com>
2015-05-04 19:40:16 +0100

    add test for singleTaskElement.

--- test/list_tasks_test.dart
+++ test/list_tasks_test.dart
@@ -1,2 +1,17 @@
 
 import 'package:ddays/list_tasks.dart';
+import 'package:test/test.dart';
+import 'dart:html';
+
+void main() {
+  test('sinlgeTaskElement returns a LIElement with correct p children', () {
+  LIElement st = singleTaskElement('task1');
+  expect(st.children[0].text, 'task1');
+  expect(st.classes.contains('singletask'),true);
+  expect(st.children[0].classes.contains('summarytext'),true);
+  LIElement st2 = singleTaskElement('');
+  expect(st2.children[0].text, '');
+  expect(st2.children[0].classes.contains('summarytext'),true);
+  });
+  
+}
