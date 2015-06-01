d0627b91db3520329128ab880df5107a2532864a
martin.haesler <martin.haesler@gmail.com>
2015-06-01 21:39:30 +0100

    Add test for taskcategory.

--- test/taskcategory_test.dart
+++ test/taskcategory_test.dart
@@ -1,3 +1,11 @@
 import 'package:test/test.dart';
 import 'package:ddays/taskcategory.dart';
 
+
+void main() {
+  test('initialization of taskcategory objects works', () {
+    TaskCategory tc1 = new TaskCategory('work', '#2c4504');
+    expect(tc1.name, 'work');
+    expect(tc1.background, '#2c4504');
+  });
+}
